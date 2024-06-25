3b581775c035c46efef53df1b49247e14d256491
danb31 <daniel.bell96@hotmail.co.uk>
2024-06-25 13:58:48 +0100

    switched to use zonal statistics

--- flood_network.py
+++ flood_network.py
@@ -1,18 +1,18 @@
-# This script assigns flood depths to each link within a network.
+# This script assigns flood depths and subsequent vehicle velocities to each
+# link within a network.
 # Inputs - cityCAT floodmap output directory, transport network
 
 import os
-import rasterio
-from rasterio.features import shapes
-import numpy as np
 import fiona
-import pyogrio
+import pyogrio  # utilised for faster exporting
+import pandas as pd
 import geopandas as gpd
 from shapely.geometry import shape
+from rasterstats import zonal_stats
 
 
 floodmap_dir = "test/data/flooding_maps_citiCAT/"
-floodmap_extension = "80min.tif"
+floodmap_extension = "40min.tif"
 network_filepath = "test/data/network/Newcastle_network_VIA.shp"
 output_dir = "test/flood_network_outputs/"
 value_threshold = 0.005
@@ -22,40 +22,7 @@ CRS = 27700
 excluded_modes = ["rail", "bus", "subway"]
 depths_column = "VALUE"
 aggregated_depths_column = "value_agg"
-
-
-def raster_pixels_to_polygons(tif_filepath, value_column):
-    with rasterio.Env():
-        with rasterio.open(tif_filepath) as src:
-            image = src.read(1).astype(np.float32)
-            results = (
-                {"properties": {value_column: v}, "geometry": s}
-                for i, (s, v) in enumerate(shapes(image, transform=src.transform))
-            )
-    geoms = list(results)
-    df = gpd.GeoDataFrame.from_features(geoms)
-    df = df.set_crs(CRS)
-    return df
-
-
-def filter_polygons(df, value_column, value_threshold):
-    return df.loc[df[value_column] >= value_threshold]
-
-
-# Checks which depth range the value is within, and raises it to the upper limit
-# Returns 9999 if above the last depth value
-def assign_depth(value_column, depths):
-    if value_column < depths[0]:
-        return depths[0]
-    for i in range(1, len(depths)):
-        if depths[i-1] < value_column <= depths[i]:
-            return depths[i]
-    return 9999
-
-
-def assign_values(df, value_column, new_column, depths):
-    df[new_column] = df[value_column].apply(lambda x: assign_depth(x, depths))
-    return df
+link_depth = "max"  # which depth of the link to use for speed calculation - "min", "max", "mean"
 
 
 def load_network(filepath):
@@ -75,12 +42,6 @@ def buffer_network(df, factor):
     return df
 
 
-def join_attribute_by_location(df_network, df_floods):
-    df_network = df_network.set_crs(CRS)
-    df_floods = df_floods.set_crs(CRS)
-    return df_network.sjoin(df_floods, how="left", predicate="intersects")
-
-
 def remove_false_positive_categories(df):
     # TODO: Remove bridges (and maybe connecting links)
     pass
@@ -88,54 +49,72 @@ def remove_false_positive_categories(df):
 
 def prepare_network():
     print("Preparing network")
-    df = load_network(network_filepath)
-    df = exlude_network_modes(df, excluded_modes)
-    return buffer_network(df, network_buffer_factor)
+    gdf = load_network(network_filepath)
+    gdf = exlude_network_modes(gdf, excluded_modes)
+    return buffer_network(gdf, network_buffer_factor)
 
 
-def process_floodmap(floodmap_filepath):
-    df = raster_pixels_to_polygons(floodmap_filepath, depths_column)
-    df = filter_polygons(df, depths_column, value_threshold)
-    return assign_values(df, depths_column, aggregated_depths_column, depths)
+def zonal_statistics(gdf, filepath):
+    print("calculating zonal statistics")
+    return gdf.join(
+        pd.DataFrame(
+            zonal_stats(
+                vectors=gdf['geometry'],
+                raster=filepath,
+                stats=["count", "min", "max", "mean", "sum", "std", "median",
+                       "majority", "minority", "unique", "range"]
+            )
+        ),
+        how='left'
+    )
 
 
-def remove_duplicates(df):
-    return df.drop_duplicates(subset=['ID', 'FRSPEED'], keep='last')
+def calculate_velocity(depth, freespeed):
+    v_kmh = (0.0009 * depth**2) - (0.5529 * depth) + 86.9448
+    v_ms = v_kmh / 3.6
+    if v_ms > freespeed:
+        return freespeed
+    return v_ms
+
+
+# Method: https://doi.org/10.1016/j.trd.2017.06.020
+def vehicle_velocity(gdf):
+    print("calculating vehicle velocities")
+    # Convert depth value from m to mm: * 1000
+    gdf["velocity"] = gdf.apply(
+        lambda row: calculate_velocity(row[link_depth]*1000, row["FRSPEED"]),
+        axis=1
+    )
+    return gdf
 
 
 def export_gpkg(df, filepath):
     print("Exporting to file: ", filepath + ".gpkg")
-    df = df[["ID", depths_column, aggregated_depths_column, "FRSPEED", "geometry"]]
     df.to_file(filepath + ".gpkg", driver="GPKG", engine="pyogrio")
 
 
 def export_csv(df, filepath):
     print("Exporting to file: ", filepath + ".csv")
-    df = df[["ID", depths_column, aggregated_depths_column, "FRSPEED"]]
     df.to_csv(filepath + ".csv", index=False)
 
 
 def main():
-    df_network = prepare_network()
+    gdf_network = prepare_network()
 
     for file in os.listdir(floodmap_dir):
         if not file.endswith(floodmap_extension):
             continue
+
         print("Processing: ", file)
 
         floodmap_filepath = floodmap_dir + file
-        output_filepath = output_dir + file.replace(".tif", "_flooded_network")
+        output_filepath = output_dir + file.replace(".tif", "_flooded_networkTEST")
 
-        try:
-            df_floods = process_floodmap(floodmap_filepath)
-            df = join_attribute_by_location(df_network, df_floods)
-            df = remove_duplicates(df)
-        except Exception as e:
-            print(f"Failed to process {file} -> {e}")
-            continue
+        gdf = zonal_statistics(gdf_network, floodmap_filepath)
+        gdf = vehicle_velocity(gdf)
 
-        export_gpkg(df, output_filepath)
-        export_csv(df, output_filepath)
+        export_gpkg(gdf, output_filepath)
+        export_csv(gdf, output_filepath)
 
     print("Done")
 
