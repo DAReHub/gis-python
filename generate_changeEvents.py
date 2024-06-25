42670b9fcca2fa56e95415fc36432307470f219d
danb31 <daniel.bell96@hotmail.co.uk>
2024-06-25 14:01:56 +0100

    new changeEvents

--- generate_changeEvents.py
+++ generate_changeEvents.py
@@ -1,17 +1,13 @@
-import os
 import pandas as pd
+from datetime import datetime, timedelta
 
-# TODO: Put paths in env file
-input_dir = "cityCAT_example_data/"
+input_dir = "test/flood_network_outputs/"
 output_xml = "test.xml"
-
-# TODO: Put params in config file
-water_depth_list = [0.05, 0.10, 0.15, 0.20, 0.25, 0.30]
-trigger_times = ["16:20:00", "16:40:00", "17:00:00", "17:20:00", "17:40:00", "18:00:00"]
-freespeed_update_list = [0.7, 0.45, 0.3, 0.2, 0.15, 0.1]
-column_categories = [9999, 0.50, 0.45, 0.40, 0.35, 0.30, 0.25, 0.20, 0.15, 0.10, 0.05]
-value_column = "value_agg"
-
+start_time = "12:00:00"
+time_interval = "00:10:00"
+time_format = "%H:%M:%S"
+id_col = "ID"
+velocity_col = "velocity"
 
 def write_headers(file):
     file.write(f'<?xml version="1.0" encoding="UTF-8"?>\n')
@@ -19,67 +15,57 @@ def write_headers(file):
         f'<networkChangeEvents xmlns="http://www.matsim.org/files/dtd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.matsim.org/files/dtd http://www.matsim.org/files/dtd/networkChangeEvents.xsd">\n\n')
 
 
-def write_starttime(file, text):
-    file.write(f'<networkChangeEvent startTime="{text}">\n')
-
-
-def write_link(file, text):
-    file.write(f'<link refId="{text}"/>\n')
-
-
-def write_scalefactor(file, text):
-    file.write(f'<freespeed type="scaleFactor" value="{text}"/>\n')
-
-
-def load_df(filepath, column):
+def load_df(filepath):
     df = pd.read_csv(filepath)
-    df = df[['ID', column]]
-    df[column] = df[column].astype('category')
-    df[column] = df[column].cat.set_categories(column_categories, ordered=True)
-    df.sort_values(['ID', column], inplace=True, ascending=True)
+    df = df[[id_col, velocity_col]]
+    df = df.drop_duplicates(id_col)
+    df = df[df[id_col].notna()]
+    df = df[df[velocity_col].notna()] # TODO: Make sure there are no nan values coming from flooded_network
+    df.sort_values(by=velocity_col, inplace=True, ascending=True)
     return df
 
 
-def unique_df(df):
-    df_unique = df.drop_duplicates('ID')
-    return df_unique[df_unique['ID'].notna()]
+def calculate_time(time1, time2, time_format):
+    t1 = datetime.strptime(time1, time_format)
+    t2 = datetime.strptime(time2, time_format)
+
+    delta1 = timedelta(hours=t1.hour, minutes=t1.minute, seconds=t1.second)
+    delta2 = timedelta(hours=t2.hour, minutes=t2.minute, seconds=t2.second)
+
+    total_delta = delta1 + delta2
+
+    total_seconds = int(total_delta.total_seconds())
+    hours, remainder = divmod(total_seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    return f"{hours:02}:{minutes:02}:{seconds:02}"
 
 
 def main():
     with open(output_xml, "w") as writefile:
         write_headers(writefile)
+        current_time = start_time
 
-        for i, csv_file in enumerate(os.listdir(input_dir)):
-            filepath = input_dir + csv_file
-            print('Processing file: ' + filepath)
+        for i in range(1, 13):
+            writefile.write(f'<networkChangeEvent startTime="{current_time}">\n')
 
-            df_links_flooded = load_df(filepath, value_column)
-            df_links_flooded_unique = unique_df(df_links_flooded)
+            filename = f"NewcastleBaseline50mm_T{i}_{i}0min_flooded_network.csv"
+            print(filename)
+            filepath = input_dir + filename
+            df = load_df(filepath)
+            grouped = df.groupby(velocity_col)
+            dfs = {value: df for value, df in grouped}
 
-            # Set initial starting time for this event stage
-            trigger_time = trigger_times[i]
+            for value, df in dfs.items():
+                links = df["ID"].to_list()
+                for link in links:
+                    writefile.write(f'<link refId="{link}"/>\n')
+                writefile.write(f'<freespeed type="absolute" value="{value}"/>\n')
+            writefile.write(f'</networkChangeEvents>\n')
 
-            for j, water_depth in enumerate(water_depth_list):
-                write_starttime(writefile, trigger_time)
-                df_unique_waterdepth = df_links_flooded_unique.loc[
-                    df_links_flooded_unique[value_column] == water_depth]
+            current_time = calculate_time(current_time, time_interval, time_format)
 
-                for k, depth in df_unique_waterdepth.iterrows():
-                    link_flooded = depth['ID']
-                    write_link(writefile, link_flooded)
-
-                write_scalefactor(writefile, freespeed_update_list[j])
-                writefile.write(f'</networkChangeEvent>\n')
-
-                # Trigger time offset by 1 second for next set of water depths -
-                #  all changes should happen at the same time for all water
-                #  depths (i.e. write_starttime(writefile, triggertimes[i]),
-                #  however this leads to an eventual MATSim error
-                trigger_time = trigger_time[:7] + str(j + 1)
-
-        writefile.write(f'</networkChangeEvents>\n')
-
-    print('END')
+    print('Done')
 
 
 if __name__ == "__main__":
